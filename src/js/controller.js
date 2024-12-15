919bea5ba07d36d6bcedc4fc1b0c90d6bdf4d6cd
akshayzagad <akshayzagade62@gmail.com.com>
2024-12-15 21:22:15 +0530

    MVP_Architecture_Structure

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-import icons from 'url:../img/icons.svg'
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
 import * as model from './model.js'
@@ -15,130 +15,36 @@ const timeout = function (s) {
   });
 };
 
-const renderSpinner = function (parentEl) {
-  const markup = `
-  <div class="spinner">
-    <svg>
-      <use href="${icons}#icon-loader"></use>
-    </svg>
-  </div> `;
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-}
-
 const recipe = async function () {
-    try {
-        const responseApi = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
+  try {
+    // Get id when click on hasmap using load hasmap event on window 
+    const id = window.location.hash.slice(1);
+    if (!id) return;
 
-        const responseData = await responseApi.json();
-        if (!responseApi.ok) throw new Error(`Somthing went wrong in responseApi.status ${response.status},${responseData.message}`);
-        // console.log(responseApi);
-        // console.log(responseData.data);
-        let { recipe } = responseData.data;
-        recipe =
-        {
-            cookingTime: recipe.cooking_time,
-            id: recipe.id,
-            imageurl: recipe.image_url,
-            publisher: recipe.publisher,
-            servings: recipe.servings,
-            sourceurl: recipe.source_url,
-            title: recipe.title,
-            ingredients: recipe.ingredients,
+    // Loading Recipe
+    await model.loadRecipe(id);
 
+    // Get import inisilize empty object in controller to use in markup
+    let { recipe } = model.state;
+    console.log(recipe);
 
-        };
-        console.log(recipe.cookingTime);
+    //  Rendering spinner when load the recipe
+    recipeView.renderSpinner();
 
-    } catch (err) {
-        alert(err)
-        console.error(err);
-    }
+    // Here we get data from model by object state which is initiallize above code
+    recipeView.render(model.state.recipe);
+    /* Here We use same method to initilize constructor in recipeView */
+    //let recipeView = new recipeView(model.state.recipe); 
 
-    const markup = `<figure class="recipe__fig">
-          <img src="${recipe.imageurl}" alt="Tomato" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
+    
+  } catch (err) {
+    alert(err)
+    console.error(err);
+  }
 
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="src/img/icons.svg#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="src/img/icons.svg#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="src/img/icons.svg#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="src/img/icons.svg#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          
-          <ul class="recipe__ingredient-list">
-        <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="src/img/icons.svg#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">0.5</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">cup</span>
-                ricotta cheese
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="src/img/icons.svg#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>
-        `;
-        recipeContainer.insertAdjacentHTML('afterbegin',markup)
 }
 
-recipe();
+// recipe();
+let events = ['hashchange', 'load']
+
+events.forEach(ev => window.addEventListener(ev, recipe));
